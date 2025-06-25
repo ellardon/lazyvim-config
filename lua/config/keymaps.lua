f5054e9c4f13143a5fbcaeb75d2f207a5a69ca14
Jonathan Tertrais <Jonathan.Tertrais@vector.com>
2025-06-25 08:44:58 +0200

    default working configuration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("v", "<leader>y", '"*y')
+vim.keymap.set("n", "<leader>p", '"*p')
+vim.keymap.set("n", "<A-Left>", "<cmd>bprevious<cr>")
+vim.keymap.set("n", "<A-Right>", "<cmd>bnext<cr>")
+
+-- Default ToggleTerm
+vim.keymap.set("n", "<leader>ft", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
+-- Press ESC twice to leave Terminal insert mode
+vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>]], { noremap = true, silent = true, desc = "Exit terminal mode" })
+-- Optional: Map <C-q> to leave terminal mode (or your preferred key)
+vim.keymap.set("t", "<C-q>", [[<C-\><C-n>]], { noremap = true, silent = true, desc = "Exit terminal mode" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.relativenumber = false
+
+vim.g.maplocalleader = "," -- Change localleader
+vim.opt.expandtab = false -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- Number of spaces tabs count for
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.wrap = true -- Wrap lines by default
