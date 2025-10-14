6e80b164f11c212906cb5ff3eb4e8a10500d5467
Jonathan Tertrais <Jonathan.Tertrais@vector.com>
2025-10-14 11:13:11 +0200

    use 'xclip' for clipboard to avoid 'wl-clipboard' windows spawning and focus issues

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,16 @@ vim.opt.expandtab = false -- Use spaces instead of tabs
 vim.opt.shiftwidth = 4 -- Number of spaces tabs count for
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
 vim.opt.wrap = true -- Wrap lines by default
+
+vim.g.clipboard = {
+  name = "xclip",
+  copy = {
+    ["+"] = { "xclip", "-selection", "clipboard" },
+    ["*"] = { "xclip", "-selection", "primary" },
+  },
+  paste = {
+    ["+"] = { "xclip", "-selection", "clipboard", "-o" },
+    ["*"] = { "xclip", "-selection", "primary", "-o" },
+  },
+  cache_enabled = 1,
+}
