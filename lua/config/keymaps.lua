35c61d61dd47345790cf5ca8bd1a07148ae5e99e
Jonathan Tertrais <jonathan.tertrais@vector.com>
2026-04-17 10:57:40 +0200

    remove useless global options and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,21 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("v", "<leader>y", '"*y')
-vim.keymap.set("n", "<leader>p", '"*p')
-vim.keymap.set("n", "<A-Left>", "<cmd>bprevious<cr>")
-vim.keymap.set("n", "<A-Right>", "<cmd>bnext<cr>")
--- Forward <A-Up>/<A-Down> to <A-k>/<A-j>
-vim.keymap.set("n", "<A-Up>", "<A-k>", { remap = true, desc = "Move Up" })
-vim.keymap.set("n", "<A-Down>", "<A-j>", { remap = true, desc = "Move Down" })
-vim.keymap.set("i", "<A-Up>", "<A-k>", { remap = true, desc = "Move Up" })
-vim.keymap.set("i", "<A-Down>", "<A-j>", { remap = true, desc = "Move Down" })
-vim.keymap.set("v", "<A-Up>", "<A-k>", { remap = true, desc = "Move Up" })
-vim.keymap.set("v", "<A-Down>", "<A-j>", { remap = true, desc = "Move Down" })
 
--- Default ToggleTerm
-vim.keymap.set("n", "<leader>ft", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
--- Press ESC twice to leave Terminal insert mode
-vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>]], { noremap = true, silent = true, desc = "Exit terminal mode" })
--- Optional: Map <C-q> to leave terminal mode (or your preferred key)
-vim.keymap.set("t", "<C-q>", [[<C-\><C-n>]], { noremap = true, silent = true, desc = "Exit terminal mode" })
+vim.keymap.set("n", "<S-Left>", "<cmd>bprevious<cr>")
+vim.keymap.set("n", "<S-Right>", "<cmd>bnext<cr>")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,20 +5,5 @@
 vim.opt.relativenumber = false
 
 vim.g.maplocalleader = "," -- Change localleader
-vim.opt.expandtab = false -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4 -- Number of spaces tabs count for
-vim.opt.tabstop = 4 -- Number of spaces tabs count for
-vim.opt.wrap = true -- Wrap lines by default
-
-vim.g.clipboard = {
-  name = "xclip",
-  copy = {
-    ["+"] = { "xclip", "-selection", "clipboard" },
-    ["*"] = { "xclip", "-selection", "primary" },
-  },
-  paste = {
-    ["+"] = { "xclip", "-selection", "clipboard", "-o" },
-    ["*"] = { "xclip", "-selection", "primary", "-o" },
-  },
-  cache_enabled = 1,
-}
+-- Activate auto-wrp
+vim.opt.wrap = true
